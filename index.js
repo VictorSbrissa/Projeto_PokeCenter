dfaad2ae91605245af646be43f5e18331f7272ad
Victor Sbrissa <victorc.sbrissa@gmail.com>
2022-08-30 19:17:53 -0300

    Atualizações
    Atualizações

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/home', function(req, res){
     res.render('../views/home');
 });
 
+app.get('/dash', function(req, res){
+    res.render('../views/dash');
+});
+
 app.listen(3001,function() {
     console.log('Executando na porta 3001');
 });
